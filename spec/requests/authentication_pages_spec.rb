95568baf5e2edd1203afb69c869f9ff449a83ece
Fumitaka Tokumitsu <toku345@gmail.com>
2014-02-18 23:07:33 +0900

    Add spec utilities.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,7 +15,7 @@ describe "Authentication" do
     before { visit signin_path }
 
     describe "with invalid information" do
-      before { click_button "Sign in"}
+      before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
