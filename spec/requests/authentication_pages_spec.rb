6207efacd2ecd0c5d8b866f1496d77ac7a4d6654
Fumitaka Tokumitsu <toku345@gmail.com>
2014-02-28 23:10:11 +0900

    Add tests for forwarding to the default page after friendly forwarding.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -63,6 +63,18 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              click_link "Sign out"
+              visit signin_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
+          end
         end
       end
 
