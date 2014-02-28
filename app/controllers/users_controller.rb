95d483e1faf44609f55dcda87ad86c3b2e5568dc
Fumitaka Tokumitsu <toku345@gmail.com>
2014-02-28 21:45:58 +0900

    Redirect to root url when signed-in user access the new and create actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :signed_in_user,     only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,       only: [:edit, :update]
+  before_action :admin_user,         only: :destroy
+  before_action :non_signed_in_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -67,5 +68,11 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def non_signed_in_user
+      if signed_in?
+        redirect_to root_url
+      end
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -114,4 +114,20 @@ describe "Authentication" do
       specify { expect(response).to redirect_to(root_url) }
     end
   end
+
+  describe "as signed-in user" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before { sign_in user, no_capybara: true }
+
+    describe "submitting a GET request to the Users#new action" do
+      before { get new_user_path }
+      specify { expect(response).to redirect_to(root_url) }
+    end
+
+    describe "submitting a GET request to the Users#create action" do
+      before { post users_path }
+      specify { expect(response).to redirect_to(root_url) }
+    end
+  end
 end
